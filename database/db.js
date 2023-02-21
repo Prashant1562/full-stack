17ee26f2c88cd22df13fb9cdb055c483ba484e9e
Prashant_Rao <prashantraogunjal1562@gmail.com>
2023-02-21 14:44:56 +0530

    backend

---
+++ database/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+mongoose.set('strictQuery', false)
+
+const Connection = async () => {
+    const URL = process.env.mongoURL;
+
+    try {
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Database Connected Succesfully');
+    } catch(error) {
+        console.log('Database cannot connected: ', error.message);
+    }
+}
+
+export default Connection;
---
+++ model/user.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema({
+    name: String,
+    username: String,
+    email: String,
+    phone: Number
+});
+
+const postUser = mongoose.model('user', userSchema);
+
+export default postUser;
